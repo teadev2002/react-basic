d5e988f88fd53e99ea3a2598a91fe1cd8b65e377
teadev2002 <anhhoanggg855@gmail.com>
2024-12-22 13:56:53 +0700

    statefull & stateless

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,31 +1,46 @@
 import React from "react";
 import Swal from "sweetalert2";
 
-class ChildComponent extends React.Component {
-  render() {
-    // let name = this.props.name;
-    // let age = this.props.age;
-    // let name = "Im child guy";
+// class ChildComponent extends React.Component {
+//   render() {
+//     let { name, age, arrJobs } = this.props;
+//     console.log("Check props\n name: ", name, " | age: ", age);
 
-    let { name, age, arrJobs } = this.props;
-    console.log("Check props\n name: ", name, " | age: ", age);
+//     return (
+//       <>
+//         <div className="first">
+//           Props: {name} - {age}
+//         </div>
+//         <div className="jobList">
+//           {arrJobs.map((item, index) => {
+//             return (
+//               <div key={item.id}>
+//                 {item.title} - {item.salary}
+//               </div>
+//             );
+//           })}
+//         </div>
+//       </>
+//     );
+//   }
+// }
 
-    return (
-      <>
-        <div className="first">
-          Props: {name} - {age}
-        </div>
-        <div className="jobList">
-          {arrJobs.map((item) => {
-            return (
-              <div key={item.id}>
-                {item.title} - {item.salary}
-              </div>
-            );
-          })}
-        </div>
-      </>
-    );
-  }
-}
+const ChildComponent = (props) => {
+  console.log("Check Child props", props);
+  let { arrJobs } = props;
+  return (
+    <>
+      <div className="list-jobs">
+        {arrJobs.map((item, index) => {
+          return (
+            <div key={item.id}>
+              {item.title} - {item.salary}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
 export default ChildComponent;
