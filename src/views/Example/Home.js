cb4958ec105e1e7716780e1ba0659098bc0882fd
teadev2002 <anhhoanggg855@gmail.com>
2024-12-25 22:42:46 +0700

    import img

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { withRouter } from "react-router";
 import { toast } from "react-toastify";
 import Color from "../HOC/Color.js";
+import corgi from "../../assets/images/1.jpg";
 class Home extends React.Component {
   componentDidMount() {
     // setTimeout(() => {
@@ -14,6 +15,9 @@ class Home extends React.Component {
     return (
       <div>
         <h1>Home hello</h1>
+        <h1>
+          <img src={corgi} />
+        </h1>
       </div>
     );
   }
